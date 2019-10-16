6968c7f042cfbe7e38ae33b936447bb38f8ab071
Nacho Perez <nacholanus@gmail.com>
2019-10-16 19:31:18 -0300

    first commit1

---
+++ computadoras.php
@@ -0,0 +1,40 @@
+<div id="computadoras" class="row">
+  <div class="col">
+<h5 id="computadora" class="titulo">
+  <span>
+    ENCONTRÁ TU COMPUTADORA
+
+
+  </span>
+
+</h5>
+</div>
+</div>
+<div id="nuevas_cartas" class="row">
+  <div id="carta" class="col-12 col-lg-6">
+    <div class="card bg-dark text-white">
+      <a href="#">
+ <img src="imagenes/computadora1.jpg" class="card-img" alt="...">
+ <div class="card-img-overlay">
+   <h4 id="carta1" class="card-title">Tus marcas favoritas</h4>
+ </div>
+</div>
+</a>
+
+
+  </div>
+  <div  id="carta" class="col-12 col-lg-6">
+    <div class="card bg-dark text-white">
+      <a href="#">
+ <img src="imagenes/computadora2.jpg" class="card-img" alt="...">
+ <div class="card-img-overlay">
+   <h4 id="carta2" class="card-title">Nuevos modelos</h4>
+ </div>
+ </a>
+</div>
+
+
+
+  </div>
+
+</div>
