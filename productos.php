cd8ea6efb993041fcc604f901986c1e884c2f51b
estebarrientos <estebarrientos@gmail.com>
2019-10-26 11:13:20 -0300

    Login actualizado en php

---
+++ productos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require "head.php"?>
+
+    <title>Galería de Productos</title>
+</head>
+<body>
+    <?php require "header.php"?>
+
+
+
+
+
+
+
+    <?php require "footer.php"?>
+</body>
+</html>
