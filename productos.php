2a8cb15e18cfae0e9e9df717575292fe9e248ee4
Lucas <l_labeau@hotmail.com>
2019-10-31 01:03:57 -0300

    Página de productos.php actualizada y algunas imágenes nuevas

--- productos.php
+++ productos.php
@@ -8,32 +8,47 @@
 <body>
     <?php require "header.php"?>
 
+    <section>
+      <h1 class="display-4">Nuestras Mochilas</h1>
+    </section>
     <div class="card-deck">
   <div class="card">
-    <img src="imagenes/mochila5.jpg" class="card-img-top" alt="Mochila Nueva">
+    <img src="imagenes/mochila5mod.jpg" class="card-img-top" alt="Imagen de mochila a la venta">
     <div class="card-body">
-      <h4 class="card-title">Card title</h4>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <h4 class="card-title">Mochila/bolso en cuero ecológico</h4>
+      <p class="card-text">
+        <b>Descripción: </b><br>
+        <i>Mochila de cuero de material sintético para mujer con acceso superior único con solapa, bolsillo interno, y tiras regulables</i>
+      </p>
+      <h5><b>Precio: $3000</b></h5>
     </div>
     <div class="card-footer">
       <small class="text-muted">Last updated 3 mins ago</small>
     </div>
   </div>
   <div class="card">
-    <img src="imagenes/mochila6" class="card-img-top" alt="...">
+    <img src="imagenes/mochilaviaje.jpg" class="card-img-top" alt="Imagen de mochila a la venta">
     <div class="card-body">
-      <h4 class="card-title">Card title</h4>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <h4 class="card-title">Mochila Alpina especial para excursiones</h4>
+      <p class="card-text">
+        <b>Descripción: </b><br>
+        <i>Mochila 100% Poliéster resistente para actividades outdoor, con acceso superior, dos bolsillos frontales, contenedores laterales, y hombreras regulables para mayor comfort</i>
+      </p>
+      <h5><b>Precio: $2500</b></h5>
     </div>
     <div class="card-footer">
       <small class="text-muted">Last updated 3 mins ago</small>
     </div>
   </div>
   <div class="card">
-    <img src="imagenes/mochila7" class="card-img-top" alt="...">
+    <img src="imagenes/mochila7.jpg" class="card-img-top" alt="Imagen de mochila a la venta">
     <div class="card-body">
-      <h4 class="card-title">Card title</h4>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <h4 class="card-title">Mochila Trekking XXL en cordura impermeable</h4>
+      <p class="card-text">
+        <b>Descripción: </b><br>
+        <i>Mochila de poliéster resistente para actividades outdoor con acceso superior, bolsillo superior chico, y dos bolsillos laterales</i>
+      </p>
+      <h5><b>Precio: $2300</b></h5>
     </div>
     <div class="card-footer">
       <small class="text-muted">Last updated 3 mins ago</small>
@@ -42,40 +57,119 @@
 </div>
 <div class="card-deck">
   <div class="card">
-    <img src="imagenes/mochila8" class="card-img-top" alt="...">
+    <img src="imagenes/mochila3.jpg" class="card-img-top" alt="Imagen de mochila a la venta">
     <div class="card-body">
-      <h4 class="card-title">Card title</h4>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <h4 class="card-title">Mochila Cartera en cuero ecológico</h4>
+      <p class="card-text">
+        <b>Descripción: </b><br>
+        <i>Mochila de cuero de material sintético unisex con acceso superior con solapa regulable, bolsillo frontal con solapa, contenedores laterales, y tiras regulables</i>
+      </p>
+      <h5><b>Precio: $3200</b></h5>
     </div>
     <div class="card-footer">
       <small class="text-muted">Last updated 3 mins ago</small>
     </div>
   </div>
   <div class="card">
-    <img src="imagenes/mochila9" class="card-img-top" alt="...">
+    <img src="imagenes/mochila9.jpg" class="card-img-top" alt="Imagen de mochila a la venta">
     <div class="card-body">
-      <h4 class="card-title">Card title</h4>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <h4 class="card-title">Mochila tipo morral en cuero combinado</h4>
+      <p class="card-text">
+        <b>Descripción: </b><br>
+        <i>Morral de cuero natural combinado de estilo con acceso superior único con solapa regulable, contenedores medianos laterales, y tira adaptable para el usuario</i>
+      </p>
+      <h5><b>Precio: $2750</b></h5>
     </div>
     <div class="card-footer">
       <small class="text-muted">Last updated 3 mins ago</small>
     </div>
   </div>
   <div class="card">
-    <img src="imagenes/mochila10" class="card-img-top" alt="...">
+    <img src="imagenes/mochilahombre-mod.jpg" class="card-img-top" alt="Imagen de mochila a la venta">
     <div class="card-body">
-      <h4 class="card-title">Card title</h4>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <h4 class="card-title">Mochila de viaje de poliéster texturizado</h4>
+      <p class="card-text">
+        <b>Descripción: </b><br>
+        <i>Mochila de viaje azul para hombre 100% poliéster con acceso principal doble de cierre de calidad excelente para transportar computadoras portátiles, bolsillo frontal, bolsillo trasero antirrobo, tiras ajustables, y contenedores laterales</i>
+      </p>
+      <h5><b>Precio: $2900</b></h5>
     </div>
     <div class="card-footer">
       <small class="text-muted">Last updated 3 mins ago</small>
     </div>
   </div>
 </div>
-
-
-
-
+<section>
+  <h1 class="display-4">Nuestras Computadoras</h1>
+</section>
+<div class="card-deck">
+  <div class="card">
+    <img src="imagenes/computador1.png" class="card-img-top" alt="Imagen de computadora a la venta">
+    <div class="card-body">
+      <h4 class="card-title">Lenovo IdeaPad S145</h4>
+      <p class="card-text">
+        <b>Descripción: </b><br>
+        <i>
+          <ul>
+            <li>Pantalla de 15.6" HD 1366x768</li>
+            <li>Diseño elegante y ligero</li>
+            <li>Procesador intel i5-8265U</li>
+            <li>Memoria de 8 GB DDR4</li>
+            <li>Windows 10 Home pre instalado</li>
+          </ul>
+        </i>
+      </p>
+      <h5><b>Precio: $54200</b></h5>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="imagenes/computador2.jpg" class="card-img-top" alt="Imagen de computadora a la venta">
+    <div class="card-body">
+      <h4 class="card-title">Notebook HP 15</h4>
+      <p class="card-text">
+        <b>Descripción: </b><br>
+        <i>
+          <ul>
+            <li>Pantalla de 15.6" HD 1366x768</li>
+            <li>Diseño práctico</li>
+            <li>Procesador AMD A4 9125 Dual-Core</li>
+            <li>Memoria de 4 GB DDR4</li>
+            <li>Windows 10 Home pre instalado</li>
+          </ul>
+        </i>
+      </p>
+      <h5><b>Precio: $31500</b></h5>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="imagenes/computador3.png" class="card-img-top" alt="Imagen de computadora a la venta">
+    <div class="card-body">
+      <h4 class="card-title">Lenovo ThinkPad T580</h4>
+      <p class="card-text">
+        <b>Descripción: </b><br>
+        <i>
+          <ul>
+            <li>Pantalla de 15.6" LED 1920x1080</li>
+            <li>Diseño moderno y elegante</li>
+            <li>Procesador Intel Core i7-8550U </li>
+            <li>Memoria de 8 GB DDR4</li>
+            <li>Windows 10 Pro 64 pre instalado</li>
+          </ul>
+        </i>
+      </p>
+      <h5><b>Precio: $115000</b></h5>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
     <?php require "footer.php"?>
      <?php include ("scripts.php") ?>
 </body>
